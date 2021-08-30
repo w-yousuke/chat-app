b8c346d01373b8ef21775a22cfed9f44cec2aab1
w-yousuke <everyday10062000@yahoo.co.jp>
2021-08-30 12:11:25 +0900

    room_usersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms. only: [:new, :create]
+  resources :rooms, only: [:new, :create]
 end
